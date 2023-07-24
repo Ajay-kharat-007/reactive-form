8bf34bf9d5b107ec9a4b1fedebd878f1a0942b9d
Ajay Kharat <ajay.kharat@antllp.com>
2023-07-24 15:09:24 +0530

    Added folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SwiperComponent } from './swiper/swiper.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SwiperComponent
   ],
   imports: [
     BrowserModule,
